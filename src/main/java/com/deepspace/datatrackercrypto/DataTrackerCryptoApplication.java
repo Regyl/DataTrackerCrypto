9d93cbf2958ea77a201831bfe569d0ada35dbfea
Новиков Евгений Алексеевич <eanovikov@phoenixit.ru>
2022-07-06 08:49:21 +0300

    @feature initial setup

---
+++ src/main/java/com/deepspace/datatrackercrypto/DataTrackerCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.deepspace.datatrackercrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTrackerCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTrackerCryptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepspace/datatrackercrypto/DataTrackerCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepspace.datatrackercrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTrackerCryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
